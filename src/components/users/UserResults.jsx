3080e062ebc86b6aa618fc8a57984f09e0e7a9ed
prajwal-asus <sharmaprajwal9016@gmail.com>
2023-09-12 22:38:12 +0530

    added components

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import Spinner from "../layout/Spinner";
+function UserResults() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetchUsers();
+  }, [loading]);
+
+  const fetchUsers = async () => {
+    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`);
+
+    const data = await response.json();
+    setUsers(data);
+    setLoading(false);
+  };
+  if (!loading) {
+    return (
+      <div className="grid grid-col-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => (
+          <h3>{user.login}</h3>
+        ))}
+      </div>
+    );
+  }else{
+    return <Spinner/>
+  }
+}
+
+export default UserResults;
