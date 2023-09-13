ee587eb0f9bbe42a668fd3b9825cf3826121aaab
prajwal-asus <sharmaprajwal9016@gmail.com>
2023-09-13 13:37:06 +0530

    ADDED context api

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,24 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect, useContext } from "react";
 import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
+import GithubContext from "../../context/GithubContext";
+
 function UserResults() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+ 
+  const {users, loading, fetchUsers} = useContext(GithubContext)
   useEffect(() => {
     fetchUsers();
-  }, [loading]);
+  },[]);
 
-  const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`);
-
-    const data = await response.json();
-    setUsers(data);
-    setLoading(false);
-  };
+  
   if (!loading) {
     return (
       <div className="grid grid-col-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
         {users.map((user) => (
-          <h3>{user.login}</h3>
+          <UserItem key={user.div} user={user}/>
         ))}
       </div>
     );
